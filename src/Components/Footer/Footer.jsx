8c6723b8da83f39b4e7063b4b3b20e23a5889eef
Bijay Jiwrajka <jiwrajkabijay@gmail.com>
2023-09-22 13:02:46 +0530

    FIX: Footer issue

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,1 +1,10 @@
+import style from "./Footer.module.scss";
+const Footer = () => {
+  return (
+    <div className={style.footer}>
+      <h2>Footer</h2>
+    </div>
+  );
+};
 
+export default Footer;
